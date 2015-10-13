58d283271b0763f8cfc978d20ef4285a27e7019f
Joe Greene <joegreene@users.noreply.github.com>
2015-10-13 11:38:24 -0700

    create cfv-main.cpp
    Added the main program portion of this week's worksheet to the week 8 folder.

---
+++ Worksheets-Projects-Solutions/Week-8/Worksheet/cfv-main.cpp
@@ -0,0 +1,102 @@
+/*
+  File: cfvmain.cpp
+  Notes:
+  - Look at the backside of this page for the questions pertaining this work
+*/
+
+
+int main()
+{
+  // 1. Create two CFloatVectors `first_list` and `second_list` with capacities 5 and 10.
+  CFloatVector first_list(5);
+  
+  
+  // 2. Push the values `1.0 1.0 2.0 3.0 5.0` to the back of `first_list` (in the order presented)
+  
+  
+  
+  
+  
+  // 3. Push the seven even numbers from 2 to 14 to the back of `second_list` (in increasing order)
+  for(float i = 2.0; i <= 14.0; i+=2.0)
+  {
+    
+  }
+  
+  // 4. Copy the contents of `second_list` to a new CFloatVector called `third_list`
+  
+  
+  // 5. Push the value `2.718` to the back of `first_list`
+  
+  
+  // 6. Print the contents of `first_list`, `second_list`, and `third_list`
+  cout << "First list:\n";
+  
+  cout << "Second list:\n";
+  
+  cout << "Third list:\n";
+  
+  
+  // 7. Print the last element of each list
+  cout << "first_list's last element: "    << 
+       << "\nsecond_list's last element: " << 
+       << "\nthird_list's last_element: "  << 
+       << endl;
+  
+  // 8. Using the methods of CFloatVector, print if `first_list` is empty, what the size and max 
+  //    capacity of `second_list` is, and if `third_list` is full
+  cout << "First list is empty: "  << 
+       << "\nSecond list's size: " << 
+       << "\nSecond list's capacity: " << 
+       << "\nThird list is full: " << 
+       << endl;
+  
+  // 9. Clear `second_list` and `third_list`, and pop the back of `first_list` twice
+  
+  
+  
+  
+  // 10. Print the contents of each list again
+  cout << "First list:\n";
+  
+  cout << "Second list:\n";
+  
+  cout << "Third list:\n";
+  
+  return 0;
+}
+
+/*
+  Questions/Problems:
+  1. Fill out cfloatvector.h and cfvmain.cpp if you have not yet.
+  
+  2. On the right of each line that produces output, write out what its output would be.
+  
+  3. In cfvmain.cpp, are any includes required? If so, which? Explain your answer.
+  
+    
+  
+  
+  
+  4. What methods, if any, would you consider useful to include with CFloatVector?
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  5. Give an example of how implementing the default constructor may look like.
+  
+  
+  
+  
+  
+  
+  
+  
+  
+*/
